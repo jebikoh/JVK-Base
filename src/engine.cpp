ab3c660fa0e63bb433e2603da221c713a99f58ca
Jayden E. <aneeshedara@gmail.com>
2025-03-01 11:12:28 -0800

    Fix initialization issue

--- src/engine.cpp
+++ src/engine.cpp
@@ -18,7 +18,6 @@ JVKEngine &JVKEngine::get() {
 }
 
 void JVKEngine::init() {
-    fmt::println("init()");
     assert(loadedEngine == nullptr);
     loadedEngine = this;
 
@@ -43,7 +42,6 @@ void JVKEngine::init() {
 }
 
 void JVKEngine::cleanup() {
-    fmt::println("cleanup()");
     if (_isInitialized) {
         destroySwapchain();
 
@@ -59,16 +57,13 @@ void JVKEngine::cleanup() {
 }
 
 void JVKEngine::draw() {
-    fmt::println("draw()");
 }
 
 void JVKEngine::run() {
-    fmt::println("run()");
     SDL_Event e;
     bool bQuit = false;
 
     while (!bQuit) {
-        fmt::println("run(): loop");
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) bQuit = true;
 
@@ -92,52 +87,54 @@ void JVKEngine::run() {
 }
 
 void JVKEngine::initVulkan() {
-    fmt::println("initVulkan()");
     // CREATE INSTANCE
-    fmt::println("initVulkan(): CREATE INSTANCE");
     vkb::InstanceBuilder builder;
-    vkb::Instance vkbInstance = builder.set_app_name("JVK")
+    auto vkbInstanceResult = builder.set_app_name("JVK")
                                         .request_validation_layers(JVK_USE_VALIDATION_LAYERS)
                                         .use_default_debug_messenger()
                                         .require_api_version(1, 3, 0)
-                                        .build()
-                                        .value();
+                                        .build();
+
+    if (!vkbInstanceResult) {
+        fmt::println("Failed to create Vulkan instance. Error: {}", vkbInstanceResult.error().message());
+        abort();
+    }
+
+    vkb::Instance vkbInstance = vkbInstanceResult.value();
 
     _instance       = vkbInstance.instance;
     _debugMessenger = vkbInstance.debug_messenger;
 
     // CREATE SURFACE
-    fmt::println("initVulkan(): CREATE SURFACE");
     SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
 
     // 1.3 FEATURES
-    fmt::println("initVulkan(): 1.3 FEATURES");
-    VkPhysicalDeviceVulkan13Features features13;
+    VkPhysicalDeviceVulkan13Features features13{};
     features13.sType            = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES;
     features13.dynamicRendering = true;
     features13.synchronization2 = true;
-    features13.pNext            = nullptr;
 
-    // 1.2 FEATURES
-    fmt::println("initVulkan(): 1.2 FEATURES");
-    VkPhysicalDeviceVulkan12Features features12;
+    VkPhysicalDeviceVulkan12Features features12{};
     features12.sType               = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES;
     features12.bufferDeviceAddress = true;
     features12.descriptorIndexing  = true;
-    features12.pNext               = nullptr;
 
     // PHYSICAL DEVICE
-    fmt::println("initVulkan(): PHYSICAL DEVICE");
     vkb::PhysicalDeviceSelector physicalDeviceBuilder{vkbInstance};
-    vkb::PhysicalDevice vkbPhysicalDevice = physicalDeviceBuilder.set_minimum_version(1, 3)
+    auto vkbPhysicalDeviceResult = physicalDeviceBuilder.set_minimum_version(1, 3)
                                                     .set_required_features_13(features13)
                                                     .set_required_features_12(features12)
                                                     .set_surface(_surface)
-                                                    .select()
-                                                    .value();
+                                                    .select();
+
+    if (!vkbPhysicalDeviceResult) {
+        fmt::println("Failed to select physical device. Error: {}", vkbPhysicalDeviceResult.error().message());
+        abort();
+    }
+
+    vkb::PhysicalDevice vkbPhysicalDevice = vkbPhysicalDeviceResult.value();
 
     // DEVICE
-    fmt::println("initVulkan(): DEVICE");
     vkb::DeviceBuilder deviceBuilder{vkbPhysicalDevice};
     vkb::Device vkbDevice = deviceBuilder.build().value();
     _device               = vkbDevice.device;
@@ -145,20 +142,16 @@ void JVKEngine::initVulkan() {
 }
 
 void JVKEngine::initSwapchain() {
-    fmt::println("initSwapchain()");
     createSwapchain(_windowExtent.width, _windowExtent.height);
 }
 
 void JVKEngine::initCommands() {
-    fmt::println("initCommands()");
 }
 
 void JVKEngine::initSyncStructures() {
-    fmt::println("initSyncStructures()");
 }
 
 void JVKEngine::createSwapchain(uint32_t width, uint32_t height) {
-    fmt::println("createSwapchain()");
     vkb::SwapchainBuilder swapchainBuilder{_chosenGPU, _device, _surface};
     _swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
 
@@ -179,7 +172,6 @@ void JVKEngine::createSwapchain(uint32_t width, uint32_t height) {
 }
 
 void JVKEngine::destroySwapchain() {
-    fmt::println("destroySwapchain()");
     vkDestroySwapchainKHR(_device, _swapchain, nullptr);
     for (int i = 0; i < _swapchainImageViews.size(); ++i) {
         vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
